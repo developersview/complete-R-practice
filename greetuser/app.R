5c47b654968cecc09078fb3aa1d7bf71284d16b0
developersview <pcslg1998@gmail.com>
2025-03-08 00:05:54 +0530

    learing R and RShiny

--- greetuser/app.R
+++ greetuser/app.R
@@ -1,17 +1,10 @@
-library(shiny)
-
-# UI Section
-ui <- fluidPage(
-  textInput("name", "What's your name?"),  # Input for the user's name
-  textOutput("greeting")                  # Output to display the greeting
-)
-
-# Server Section
-server <- function(input, output, session) {
-  output$greeting <- renderText({          # Render a greeting message
-    paste0("Hello ", input$name)
-  })
-}
-
-# Run the app
-shinyApp(ui, server)
+tableOutput("mortgage")
+output$greeting <- renderText({
+  paste0("Hello ", input$name)
+})
+numericInput("age", "How old are you?", value = NA)
+textInput("name", "What's your name?")
+textOutput("greeting")
+output$histogram <- renderPlot({
+  hist(rnorm(1000))
+}, res = 96)
